30ee5bd5c5fe45c8dfb3f6c43d6f5fc62ff5cbee
Srinivas <seenuchaparala@gmail.com>
2022-03-08 16:07:59 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+
+
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+    alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert); 
 }
+
+
+
+addingEventListener();
